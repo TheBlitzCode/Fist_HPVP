e252c9266638c67345c1b8a15e04ec518b7465b0
SrDxni <57044930+SrDxni@users.noreply.github.com>
2019-11-05 15:04:37 -0600

    Update Fist.php

--- src/SrDxni_TheBlitzCode/Fist.php
+++ src/SrDxni_TheBlitzCode/Fist.php
@@ -8,8 +8,20 @@ namespace: SrDxni_TheBlitzCode;
 
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener:
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\command\{CommandSender, Command};
+use pocketmine\utils\TextFormat as T;
+use pocketmine\utils\Config;
 
 class Fist extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getLogger()->info("Plugin Fist Enable");
+   }
+  
+  public function onCommand(CommandSender $pl, Command $cmd, $label, array $args): bool {
+    switch($cmd->getName()){
+        case "fist":
 }
 ?>
