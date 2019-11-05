a227de5eb596d9958895117ec62a9de46c5f606a
TheBlitzCode <56805984+TheBlitzCode@users.noreply.github.com>
2019-11-05 21:54:51 +0100

    Update Fist.php

--- src/SrDxni_TheBlitzCode/Fist.php
+++ src/SrDxni_TheBlitzCode/Fist.php
@@ -3,4 +3,13 @@
  * This pkugins is created for SrDxni and TheBlitzCode
  * is only for HardPvP
 */
+
+namespace: SrDxni_TheBlitzCode;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener:
+
+class Fist extends PluginBase implements Listener{
+}
 ?>
