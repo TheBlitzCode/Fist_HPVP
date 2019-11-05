38f0050a4366baabb35fafb63c3db4e47db13410
TheBlitzCode <56805984+TheBlitzCode@users.noreply.github.com>
2019-11-05 22:32:59 +0100

    Update Fist.php

--- src/SrDxni_TheBlitzCode/Fist.php
+++ src/SrDxni_TheBlitzCode/Fist.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This pkugins is created for SrDxni and TheBlitzCode
+ * This plugins is created for SrDxni and TheBlitzCode
  * is only for HardPvP
 */
 
@@ -13,19 +13,42 @@ use pocketmine\item\Item;
 use pocketmine\command\{CommandSender, Command};
 use pocketmine\utils\TextFormat as T;
 use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\Server;
+use jojoe77777\FormAPI\SimpleForm;
 
 class Fist extends PluginBase implements Listener{
   
   public function onEnable(){
     $this->getLogger()->info("Plugin Fist Enable");
+    $this->checkPlugin("FormAPI");
   }
   
+  //comprueba plugin
   public function checkPlugin(string $plugin){
     if (!$this->getServee()->getPluginManager()->getPlugin() == $plugin){
       $this->getLogger()->notice(base64_decode("Tm8gc2VhIGVuY29udHJhZG8gZWwgcGx1Z2luIA==").plugin);
     } else {
       return $plugin;
       $this->getLogger()->notice(base64_decode("RWwgcGx1Z2luIA==").$plugin.base64_decode(" Sea cargado corectamente"));
+    }
+  }
+  
+  public funtion deathUI($player){
+    if ($player instanceof Player){
+      $form = new SimpleForm(function (Player $player, $d){
+        if ($d !== null){
+          switch($d){
+            case "0":
+              $this->getServer()->dispatchCommand($player, "/fist join");
+            break;
+            case "1":
+              $lobby = $this->getServer()->getDefaultLevel()->getSafeSpawn();
+              $player->teleport($lobby);
+            break;
+          }
+        }
+      });
     }
   }
   
