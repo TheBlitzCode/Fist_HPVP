ce8d9a47599f75d8848e88967295469d618410c9
TheBlitzCode <56805984+TheBlitzCode@users.noreply.github.com>
2019-11-05 22:18:46 +0100

    Update Fist.php

--- src/SrDxni_TheBlitzCode/Fist.php
+++ src/SrDxni_TheBlitzCode/Fist.php
@@ -18,7 +18,16 @@ class Fist extends PluginBase implements Listener{
   
   public function onEnable(){
     $this->getLogger()->info("Plugin Fist Enable");
-   }
+  }
+  
+  public function checkPlugin(string $plugin){
+    if (!$this->getServee()->getPluginManager()->getPlugin() == $plugin){
+      $this->getLogger()->notice(base64_decode("Tm8gc2VhIGVuY29udHJhZG8gZWwgcGx1Z2luIA==").plugin);
+    } else {
+      return $plugin;
+      $this->getLogger()->notice(base64_decode("RWwgcGx1Z2luIA==").$plugin.base64_decode(" Sea cargado corectamente"));
+    }
+  }
   
   public function onCommand(CommandSender $pl, Command $cmd, $label, array $args): bool {
     switch($cmd->getName()){
